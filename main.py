0744848f31249b10d56dc32bd78fe84dbbd4c4c4
joseph skokan <joeskokan20@gmail.com>
2024-12-31 01:33:12 -0500

    gunicorn

--- main.py
+++ main.py
@@ -85,6 +85,6 @@ def update_graph(selected_category):
     ])
     
     return bar_fig, pie_fig, line_fig, scatter_fig, avg_display
-
+server = app.server
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0', port=8050, debug=True)
